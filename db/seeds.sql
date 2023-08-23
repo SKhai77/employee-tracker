2ce3e0f7673f229186f5d08fff1c2038d948b68f
SKhai77 <orionkhai@gmail.com>
2023-08-23 16:51:43 -0500

    Update and completed by creating all the required tables and their contents with comments added

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,29 @@
-INSERT INTO movies (movie_name)
-VALUES ("Lion King"),
-       ("The Godfather"),
-       ("West Side Story"),
-       ("Parasite"),
-       ("The Wizard of Oz");
+-- Inserts the names of different departments into department table
+INSERT INTO department (name)
+VALUES ("Engineering"), -- Every tables has their own individual id starting from number 1 and the number will Increase Automatically since it is made to do AUTO_INCREMENT in the Schema.sql file.
+       ("Finance"), -- Department ID 2. Because this is in the Second line of this department table.
+       ("Legal"), -- Department ID 3. Because this is in the Third line of this department table.
+       ("Sales"); -- Department ID 4. Because this is in the Fourth line of this department table.
 
-INSERT INTO reviews (movie_id, review)
-VALUES (1, "Zazu is underrated. Give that hornbill a sequel!"),
-       (2, "I'm gonna make him an offer you can't refuse, watch this movie"),
-       (1, "Scar is the lion everyone loves to hate"),
-       (3, "Ten years of ballet and three years of tap to join a gang in this neighborhood"),
-       (5, "The tin man gave a metallic, hollow performance"),
-       (1, "Hakuna matata"),
-       (5, "Those flying monkeys are nightmare fuel!");
+-- Inserts the title, salary, and department id of employee role into role table
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant Manager", 160000, 2), -- The depmart_id is '2' as the Accountant Manager is under Finance which is in the "second line" of the "department" table (see above "department table).
+       ("Accountant", 125000, 2), -- This position is under Finance and the id is 2 as it is in the second line of the department table.
+       ("Legal Team Lead", 250000, 3), -- This position is under Legal and the id is 3 as it is in the Third line of the department table.
+       ("Lawyer", 190000, 3), -- This position is under Legal and the id is 3 as it is in the Third line of the department table.
+       ("Lead Engineer", 150000, 1), -- This position is under Enginnering and the id is 1 as it is in the First line of the department table.
+       ("Software Engineer", 100000, 1), -- This position is under Enginnering and the id is 1 as it is in the First line of the department table.
+       ("Sales Lead", 100000, 4), -- This position is under Sales and the id is 4 as it is in the Fourth line of the department table.
+       ("Salesperson", 80000, 4); -- This position is under Sales and the id is 4 as it is in the Fourth line of the department table.
        
+-- Inserts the employees' first name, last name, role id, and manager id into employee table
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, NULL), -- John Doe is assigned as 'Accountant Manager' which is under the 'role' table in the very first(1) line of the table(see above "role" table). That is why the "role_id" is '1'. "NULL" means he has no manager.
+       ("Mike", "Chan", 2, 1) -- Assigned as an 'Accountant' and the id is '2'. His manager is John Doe and the id is '1'.
+       ("Tom", "Allen", 3, NULL), -- Assigned as a Legal Team Lead
+       ("Kunal", "Singh", 4, 3), -- Assigned as a Lawyer
+       ("Malia", "Brown", 5, NULL), -- Assigned as Lead Engineer
+       ("Ashley", "Rodriguez", 6, 5), -- Assigned as Software Engineer
+       ("Sam", "Kash", 6, NULL), -- Assigned as Sales Lead
+       ("Kevin", "Tupik", 7, 6); -- Assigned as Salesperson
+
