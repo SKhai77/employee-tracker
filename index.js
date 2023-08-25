9489c31977c6b336713b7a30d08d7015de45e185
SKhai77 <orionkhai@gmail.com>
2023-08-25 12:17:01 -0500

    Added code for add employee function and still need to be fixed

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const inquirer = require('inquirer');
 // const routes = require('./db/index');
 const db = require('./db/connection');
 
+require("console.table");
+
+
 // Create an array of tasks for user input
 const taskList = [
   {
     type: 'list',
     message: 'What would you like to do?',
-    choices: ['View All Departments', 'Add Department', 'View All Roles', 'Add Role', 'View All Employees', 'Add Employee', 'Update Employee Role', 'Exit'],
+    choices: ['View All Departments', 'Add Department', 'View All Roles', 'Add Role', 'View All Employees', 'Add Employee', 'Update Employee Role', 'View Employee By Department', 'View Employee by Manager', 'Exit'],
     name: 'task',
   },
 ];
@@ -46,6 +49,14 @@ function init() {
 
         case 'Update Employee Role':
           updateEmployeeRole();
+          break;
+
+        case 'View Employees by Department':
+          viewEmployeeByDepartment();
+          break;
+
+        case 'View Employee by Manager':
+          ViewEmployeeByManager();
           break;
 
         default:
@@ -116,10 +127,36 @@ function viewAllEmployees() {
 };
 
 function addEmployee() {
-  db.query('SELECT * FROM department', (error, response) => {
+inquirer
+.prompt([
+  {
+    type: 'input',
+    name: 'first_name',
+    message: 'Enter first name:',
+  },
+  {
+    type: 'input',
+    name: 'last_name',
+    message: 'Enter last name:',
+  },
+  {
+    type: 'input',
+    name: 'role_id',
+    message: 'Enter the name of the role:',
+  },
+  {
+    type: 'input',
+    name: 'department_id',
+    message: 'Enter the name of the department:',
+  },
+]);
+
+  db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`, (error, response) => {
     console.table(response)
+    if (error) throw error;
+    console.log('Employee added successfully.');
     // Function call to initialize app
-    init()
+    init();
   })
 };
 
@@ -127,7 +164,23 @@ function updateEmployeeRole() {
   db.query('SELECT * FROM department', (error, response) => {
     console.table(response)
     // Function call to initialize app
-    init()
+    init();
+  })
+};
+
+function viewEmployeeByDepartment() {
+  db.query('SELECT d.name AS department, e.first_name, e.last_name, r.title AS role FROM employee e JOIN role r ON e.role_id = r.role_id JOIN department d ON r.department_id = d.department_id ORDER BY d.name, e.last_name, e.first_name', (error, response) => {
+    console.table(response)
+    // Function call to initialize app
+    init();
+  })
+};
+
+function ViewEmployeeByManager() {
+  db.query('SELECT * FROM department', (error, response) => {
+    console.table(response)
+    // Function call to initialize app
+    init();
   })
 };
 
