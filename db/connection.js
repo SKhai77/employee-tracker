1f063a443741ea915bc7148f2cd53affeb79d85f
SKhai77 <orionkhai@gmail.com>
2023-08-30 18:26:11 -0500

    Added a connectin to the database using promises

--- db/connection.js
+++ db/connection.js
@@ -2,18 +2,22 @@
 const mysql = require('mysql2');
 
 // Connect to database
-const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // MySQL username,
-      user: 'root',
-      // Add MySQL password here
-      password: 'mypass',
-      database: 'employees_db'
-    },
-    // Print a message indicating a successful database connection
-    console.log(`Connected to the employees_db database.`)
-  );
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'mypass',
+  database: 'employees_db'
+});
 
-//   Export the "db" variable
-  module.exports = db;
+// Print a message indicating a successful database connection
+db.promise()
+  .connect()
+  .then(() => {
+    console.log(`Connected to the employees_db database.\n Welcome to Employee Tracking System!`);
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database:', error);
+  });
+
+// Export the "db" variable
+module.exports = db;
